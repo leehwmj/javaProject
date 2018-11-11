ee7a8397ca2342bdf86ca02a39aeade5676e64fb
MooJu <leehwmj@core.today>
2018-11-12 01:54:26 +0900

    java example of basic

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("드디어 된다");
+        System.out.println(1+1);
+        System.out.println(1.2+1.5);
+
+        int a;
+        String b;
+
+        a = 5484;
+        b = "hello";
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ src/StringExample.java
@@ -0,0 +1,8 @@
+public class StringExample {
+    public static void main(String args[]){
+        String a = "hello", b = "java", c = "hello";
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+    }
+}
