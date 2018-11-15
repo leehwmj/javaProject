4c59668c3a002a53905429e751d6c805ca0cada9
MooJu <leehwmj@core.today>
2018-11-15 20:48:38 +0900

    Call by Value
    method에서 객체를 받아 그 객체 내부의

---
+++ src/callByValue/Counter.java
@@ -0,0 +1,18 @@
+package callByValue;
+
+class Updater {
+    public void update(Counter counter) {
+        counter.count++;
+    }
+}
+
+public class Counter {
+    int count = 0;
+    public static void main (String[] args) {
+        Counter myCounter = new Counter();
+        System.out.println("before update:"+myCounter.count);
+        Updater myUpdater = new Updater();
+        myUpdater.update(myCounter);
+        System.out.println("after update:"+myCounter.count);
+    }
+}
